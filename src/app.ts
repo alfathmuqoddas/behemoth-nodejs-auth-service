0240d3befda487c796b3904e843dc38192599025
Alfath Muqoddas <alfath.muqoddas@gmail.com>
2025-12-22 15:34:14 +0700

    update: add health probes endpoint

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response } from "express";
 import pinoHttp from "pino-http";
 import authRoutes from "./routes/authRoutes";
 import protectedRoutes from "./routes/protectedRoutes";
+import healthRoutes from "./routes/healthRoutes";
 import logger from "./config/logger";
 import { register } from "./config/metrics";
 import { metricsMiddleware } from "./middleware/metricsMiddleware";
@@ -20,5 +21,6 @@ app.get("/metrics", async (req: Request, res: Response) => {
 
 app.use("/", authRoutes);
 app.use("/protected", protectedRoutes);
+app.use("/health", healthRoutes);
 
 export default app;
